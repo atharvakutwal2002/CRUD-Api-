917eb66a3c6055ed0db37357baebeb1ad48cc83f
Atharva <atharvakutwal2002@gmail.com>
2022-08-29 21:41:10 +0530

    delete request added

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,39 @@
 import express from "express";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const user = [];
+let user = [];
 
-router.get('/', (req, res) => {
-    console.log(user)
+router.get("/", (req, res) => {
+  console.log(user);
+  res.send(user);
+});
+
+router.post("/", (req, res) => {
+  const id = uuidv4();
+  const newUser = req.body;
+  const finalUser = { ...newUser, id: id };
+  user.push(finalUser);
+  res.send(finalUser);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const founduser = user.find((user) => user.id == id);
+  res.send(founduser);
+});
+
+router.delete('/:id',(req,res)=>{
+    const id= req.params.id;
+
+    user= user.filter((user)=>{
+        user.id!==id;
+    })
+
     res.send(user);
-});
-
-router.post('/',(req,res)=>{
-    const id = uuidv4();
-    const newUser= req.body;
-    const finalUser = {...newUser, id:id}
-    user.push(finalUser);
-    res.send(finalUser);
-
-});
-
-router.get('/user',(req, res)=>{
-
 })
+
+router.patch()
 
 export default router;
